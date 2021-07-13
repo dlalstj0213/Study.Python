edb05639fe8cbea4eb06e7119844e5bc18baf952
dlalstj0213 <dlalstj0213@gmail.com>
2021-07-13 22:36:28 +0900

    variable

---
+++ data_type/variable/variable.py
@@ -0,0 +1,20 @@
+val = 1234
+print(type(val))
+
+val = "1234"
+print(type(val))
+
+val = True
+print(type(val))
+
+val = 100
+val += 10
+val += 20
+val += 30
+print("number :", val)
+
+val = "문자열입니다"
+val += "!"
+val *= 2
+print("string : " + val)
+print("string :", val)
