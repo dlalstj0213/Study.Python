d89485b638d8ef0e7908316715e445587a2cd75c
dlalstj0213 <dlalstj0213@gmail.com>
2021-08-12 02:18:57 +0900

    making pyqt5 module

--- my_pyqt5/main.py
+++ my_pyqt5/main.py
@@ -1,18 +1,53 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.uic.properties import QtWidgets
 
-form_class = uic.loadUiType("main.ui")[0]
+
+class View1(QDialog):
+    def __init__(self):
+        super(View1, self).__init__()
+        uic.loadUi("view/view_1.ui", self)
 
 
-class MyWindow(QMainWindow, form_class):
+class View2(QDialog):
+    def __init__(self):
+        super(View2, self).__init__()
+        uic.loadUi("view/view_2.ui", self)
+
+
+# 윈도우 클래스 선언
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi("ui_design.ui", self)
+        # self.btn_naver.clicked.connect(lambda: self.goto(self.btn_naver))
+        self.connect_events()
+
+    def connect_events(self):
+        for widget in self.sidebar.findChildren(QPushButton):
+            print(
+                widget.objectName()
+            )  # 위젯의 오브젝트 이름까지 접근했음 이제 해당 파일의 View만 어떻게 연결시켜서 불러오면됨
+
+    def go_to(self, btn_obj):
+        print(str(btn_obj.__name__))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = MyWindow()
+    myWindow = MainWindow()
+    # myWindow.section_body.addWidget(View1())
+
+    widget = QStackedWidget()
+    view1 = View2()
+    widget.addWidget(view1)
+    myWindow.section_layout.addWidget(widget)
+
     myWindow.show()
-    app.exec_()
+    try:
+        sys.exit(app.exec_())
+    except BaseException as be:
+        print("Error Type:", be.__class__.__name__)
+        print(be)
+        print("Exiting...!!!")
---
+++ my_pyqt5/main_play.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("main.ui")[0]
+
+
+class Form(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.ui = uic.loadUi("view/status.ui")
+        self.ui.setupUi(self)
+
+
+# 윈도우 클래스 선언
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # UI 불러오기
+        self.ui = uic.loadUi("main.ui", self)
+        # 시그널(signal) - 슬롯 연결
+        self.btn_1.clicked.connect(self.print_one)
+        print(self.main)
+        self.main_layout.addWidget(Form(self))
+        print("Button:", self.btn_1)
+
+    def print_one(self):
+        print("Clicked!!!")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
