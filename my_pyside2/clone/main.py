f043321402aaacfe032eb8e2a051fd1b044f750d
dlalstj0213 <dlalstj0213@gmail.com>
2021-08-17 23:15:02 +0900

    add progressBar

--- my_pyside2/clone/main.py
+++ my_pyside2/clone/main.py
@@ -8,6 +8,9 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from ui_loading_window import *
 
+# Global value
+progressBarValue = 0
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -29,6 +32,64 @@ class MainWindow(QMainWindow):
         self.shadow.setColor(QColor(0, 92, 157, 150))
         # Appy shadow to central widget
         self.ui.centralwidget.setGraphicsEffect(self.shadow)
+
+        # code QTIMER to delay the progressBar
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.appProgress)
+        # Time interval in Milliseconds for the progressbar to change value
+        self.timer.start(100)
+
+    # appProgress fuction : update the progressBar value
+    def appProgress(self):
+        global progressBarValue
+        # Apply progressBarValue to the progressBar
+        self.ui.my_progressBar.setValue(progressBarValue)
+
+        # View progressBarValue and update status text or close splash screen and open main window
+        loading_progress_status_text = "Please wait..."
+        loading_status_text = "Initializing Desk App"
+        if progressBarValue > 100:
+
+            # reset / stop timer
+            self.timer.stop()
+
+            # Close the splash screen after the progress is complete
+            # self.close()
+
+            loading_progress_status_text = "Loading completed successfully"
+            loading_status_text = "Now Open Desk App"
+        elif progressBarValue < 10:
+            pass
+        elif progressBarValue < 20:
+            loading_progress_status_text = "Connecting to App server"
+        elif progressBarValue < 35:
+            loading_progress_status_text = "Logging in..."
+        elif progressBarValue < 55:
+            loading_progress_status_text = "Logging in succesfully. Requesting Data..."
+        elif progressBarValue < 65:
+            loading_progress_status_text = (
+                "Data set to design. Requesting design data information"
+            )
+        elif progressBarValue < 85:
+            loading_progress_status_text = "Finalizing data setup"
+        else:
+            loading_progress_status_text = "Almost done..."
+            loading_status_text = "Minseo Design"
+
+        # change the loading status text
+        QtCore.QTimer.singleShot(
+            0,
+            lambda: self.ui.loading_progress_status.setText(
+                loading_progress_status_text
+            ),
+        )
+        QtCore.QTimer.singleShot(
+            0,
+            lambda: self.ui.loading_status.setText(loading_status_text),
+        )
+
+        # increase progressBarValue by 1 after every time interval we set of 100 milliseconds;
+        progressBarValue += 1
 
 
 # Execute
--- my_pyside2/clone/ui_loading_window.py
+++ my_pyside2/clone/ui_loading_window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'loading_windowMciGJW.ui'
+## Form generated from reading UI file 'loading_windowgJgdSp.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -14,12 +14,6 @@ from PySide2.QtWidgets import *
 
 import python_app_rc
 
-# Conver qrc resource file to python resource file
-# os.system("pyrcc5 python_app.qrc -o python_app_rc.py")
-# pyrcc4 -o resources.py resources.qrc
-import python_app_rc
-
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
@@ -30,34 +24,28 @@ class Ui_MainWindow(object):
         self.main_bg_frame = QFrame(self.centralwidget)
         self.main_bg_frame.setObjectName(u"main_bg_frame")
         self.main_bg_frame.setGeometry(QRect(10, 89, 600, 400))
-        self.main_bg_frame.setStyleSheet(
-            u"border-radius: 20px;\n"
-            "background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:1, y2:1, stop:0 rgba(55, 115, 165, 255), stop:1 rgba(255, 255, 255, 255));\n"
-            "border-bottom: 5px solid rgb(255, 209, 65);\n"
-            "border-right: 5px solid rgb(54, 110, 156);\n"
-            "border-top: 5px solid rgb(54, 110, 156);\n"
-            "border-left: 5px solid rgb(255, 209, 65);"
-        )
+        self.main_bg_frame.setStyleSheet(u"border-radius: 20px;\n"
+"background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:1, y2:1, stop:0 rgba(55, 115, 165, 255), stop:1 rgba(255, 255, 255, 255));\n"
+"border-bottom: 5px solid rgb(255, 209, 65);\n"
+"border-right: 5px solid rgb(54, 110, 156);\n"
+"border-top: 5px solid rgb(54, 110, 156);\n"
+"border-left: 5px solid rgb(255, 209, 65);")
         self.main_bg_frame.setFrameShape(QFrame.StyledPanel)
         self.main_bg_frame.setFrameShadow(QFrame.Raised)
         self.logo_frame = QFrame(self.main_bg_frame)
         self.logo_frame.setObjectName(u"logo_frame")
         self.logo_frame.setGeometry(QRect(0, 0, 181, 151))
-        self.logo_frame.setStyleSheet(
-            u"image: url(:/images/image/python_logo.png);\n"
-            "background-color: none;\n"
-            "border: none"
-        )
+        self.logo_frame.setStyleSheet(u"image: url(:/images/image/python_logo.png);\n"
+"background-color: none;\n"
+"border: none")
         self.logo_frame.setFrameShape(QFrame.StyledPanel)
         self.logo_frame.setFrameShadow(QFrame.Raised)
         self.middle_line_first = QFrame(self.main_bg_frame)
         self.middle_line_first.setObjectName(u"middle_line_first")
         self.middle_line_first.setGeometry(QRect(70, 20, 600, 600))
-        self.middle_line_first.setStyleSheet(
-            u"background-color: none;\n"
-            "border: 10px solid rgb(51, 106, 150);\n"
-            "border-radius: 300px;"
-        )
+        self.middle_line_first.setStyleSheet(u"background-color: none;\n"
+"border: 10px solid rgb(51, 106, 150);\n"
+"border-radius: 300px;")
         self.middle_line_first.setFrameShape(QFrame.StyledPanel)
         self.middle_line_first.setFrameShadow(QFrame.Raised)
         self.welcome_label = QLabel(self.main_bg_frame)
@@ -69,39 +57,42 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(75)
         self.welcome_label.setFont(font)
-        self.welcome_label.setStyleSheet(u"border: none;\n" "background-color: none;")
+        self.welcome_label.setStyleSheet(u"border: none;\n"
+"background-color: none;")
         self.welcome_label.setAlignment(Qt.AlignCenter)
         self.more_info_frame = QFrame(self.main_bg_frame)
         self.more_info_frame.setObjectName(u"more_info_frame")
         self.more_info_frame.setGeometry(QRect(120, 200, 481, 201))
-        self.more_info_frame.setStyleSheet(
-            u"background-color: rgb(51, 106, 150);\n"
-            "border-bottom: 5px solid rgb(255, 209, 65);\n"
-            "border-top: 5px solid rgb(54, 110, 156);\n"
-            "border-right: 5px solid rgb(255, 209, 65);\n"
-            "border-left: 5px solid rgb(255, 209, 65);\n"
-            "border-radius: 20px;"
-        )
+        self.more_info_frame.setStyleSheet(u"background-color: rgb(51, 106, 150);\n"
+"border-bottom: 5px solid rgb(255, 209, 65);\n"
+"border-top: 5px solid rgb(54, 110, 156);\n"
+"border-right: 5px solid rgb(255, 209, 65);\n"
+"border-left: 5px solid rgb(255, 209, 65);\n"
+"border-radius: 20px;")
         self.more_info_frame.setFrameShape(QFrame.StyledPanel)
         self.more_info_frame.setFrameShadow(QFrame.Raised)
-        self.label = QLabel(self.more_info_frame)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 10, 461, 51))
+        self.loading_status = QLabel(self.more_info_frame)
+        self.loading_status.setObjectName(u"loading_status")
+        self.loading_status.setGeometry(QRect(10, 10, 461, 51))
         font1 = QFont()
         font1.setFamily(u"Consolas")
         font1.setPointSize(20)
         font1.setBold(True)
         font1.setWeight(75)
-        self.label.setFont(font1)
-        self.label.setStyleSheet(u"border:none;\n" "color:rgb(255, 255, 255);")
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label_2 = QLabel(self.more_info_frame)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(310, 60, 141, 51))
+        self.loading_status.setFont(font1)
+        self.loading_status.setStyleSheet(u"border:none;\n"
+"color:rgb(255, 255, 255);")
+        self.loading_status.setAlignment(Qt.AlignCenter)
+        self.loading_progress_status = QLabel(self.more_info_frame)
+        self.loading_progress_status.setObjectName(u"loading_progress_status")
+        self.loading_progress_status.setGeometry(QRect(320, 70, 141, 41))
         font2 = QFont()
         font2.setFamily(u"Consolas")
-        self.label_2.setFont(font2)
-        self.label_2.setStyleSheet(u"border:none;\n" "color:rgb(255, 255, 255);")
+        self.loading_progress_status.setFont(font2)
+        self.loading_progress_status.setStyleSheet(u"border:none;\n"
+"color:rgb(255, 255, 255);")
+        self.loading_progress_status.setAlignment(Qt.AlignCenter)
+        self.loading_progress_status.setWordWrap(True)
         self.credit_label = QLabel(self.more_info_frame)
         self.credit_label.setObjectName(u"credit_label")
         self.credit_label.setGeometry(QRect(20, 150, 221, 41))
@@ -110,7 +101,23 @@ class Ui_MainWindow(object):
         font3.setBold(True)
         font3.setWeight(75)
         self.credit_label.setFont(font3)
-        self.credit_label.setStyleSheet(u"border:none;\n" "color: rgb(255, 255, 255);")
+        self.credit_label.setStyleSheet(u"border:none;\n"
+"color: rgb(255, 255, 255);")
+        self.my_progressBar = QProgressBar(self.more_info_frame)
+        self.my_progressBar.setObjectName(u"my_progressBar")
+        self.my_progressBar.setGeometry(QRect(20, 80, 301, 21))
+        self.my_progressBar.setStyleSheet(u"QProgressBar {\n"
+"	background-color:rgb(51, 106, 150);\n"
+"	border-style:none;\n"
+"	text-align:center;\n"
+"	color: rgb(255, 0, 0);\n"
+"}\n"
+"\n"
+"QProgressBar::chunk{\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:1, y2:1, stop:0 rgba(79, 162, 230, 255), stop:1 rgba(255, 255, 255, 255));\n"
+"	border-radius: 10px;\n"
+"}")
+        self.my_progressBar.setValue(24)
         self.python_frame = QFrame(self.centralwidget)
         self.python_frame.setObjectName(u"python_frame")
         self.python_frame.setGeometry(QRect(480, 10, 250, 250))
@@ -122,26 +129,13 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
-
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(
-            QCoreApplication.translate("MainWindow", u"MainWindow", None)
-        )
-        self.welcome_label.setText(
-            QCoreApplication.translate("MainWindow", u"WELCOME", None)
-        )
-        self.label.setText(
-            QCoreApplication.translate(
-                "MainWindow", u"News&Jobs Collector-Desk App", None
-            )
-        )
-        self.label_2.setText(
-            QCoreApplication.translate("MainWindow", u"Please wait...", None)
-        )
-        self.credit_label.setText(
-            QCoreApplication.translate("MainWindow", u"Designed by Minseo Rhie", None)
-        )
-
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.welcome_label.setText(QCoreApplication.translate("MainWindow", u"WELCOME", None))
+        self.loading_status.setText(QCoreApplication.translate("MainWindow", u"Initializing Desk App", None))
+        self.loading_progress_status.setText(QCoreApplication.translate("MainWindow", u"Please wait...", None))
+        self.credit_label.setText(QCoreApplication.translate("MainWindow", u"Designed by Minseo Rhie", None))
     # retranslateUi
+
